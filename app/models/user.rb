945e68de3b892c1a311d6dc5cc8fadde33bd0fda
Joel Roggeman <joelroggeman@gmail.com>
2016-01-27 14:45:33 -0500

    User validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true
+
+  # Should have password and confirmation when creating user
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user should save" do
+    user = User.new
+
+    user.password = "123456"
+    user.password_confirmation = "123456"
+    user.name = "bob"
+    user.username = "bob"
+
+    assert user.save
+  end
+
+  test "empty user should not save" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "user should not save without a password" do
+    user = User.new
+    user.username = "becca"
+    user.name = "becca"
+    assert_not user.save
+  end
+
+  test "user should not save without a username" do
+    user = User.new
+    user.name = "bob"
+    user.password = "1234"
+    user.password_confirmation = "1234"
+
+    assert_not user.save
+  end
+
+  test "user should not save without a name" do
+    user = User.new
+
+    user.username = "bob"
+    user.password = "1234"
+    user.password_confirmation = "1234"
+
+    assert_not user.save
+  end
+
+  test "user should not save without both passwords" do
+    user = User.new
+    user.password = "1234"
+    user.username = "bob"
+    user.name = "bob"
+
+    assert_not user.save
+  end
+
+  test "user should have six or more chars in password" do
+    user = User.new
+
+    user.password = "1234"
+    user.password_confirmation = "1234"
+    user.name = "bob"
+    user.username = "bob"
+
+    assert_not user.save
+  end
 end

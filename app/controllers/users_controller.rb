88016467776c3e2b85548b2fee116465a1acb45a
Joel Roggeman <joelroggeman@gmail.com>
2016-01-27 15:41:15 -0500

    Added user creation and few infrastructure for temporary testing and dev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
+    @user = User.new
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      flash[:success] = "Welcome to Examinatr :)"
+      redirect_to user
+    else
+      flash[:error] = "Problem saving user"
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def index
-  end
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation, :name)
+    end
 end

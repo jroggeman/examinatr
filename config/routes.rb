b65c89204d11e65e8a29a45c514b3ad532265813
Joel Roggeman <joelroggeman@gmail.com>
2016-02-01 18:11:05 -0500

    Clean up routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'exams/index'
-
-  get 'exams/new'
-
-  get 'exams/create'
-
-  get 'exams/show'
-
+  # Users and exams
   resources :users, except: [:edit, :update, :destroy]
+  resources :exams, except: [:edit, :update, :destroy]
 
+  # Session management
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'

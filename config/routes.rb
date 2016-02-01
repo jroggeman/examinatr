245668f8bd3edea2c08bdb41a699c15ce6b0c75d
Joel Roggeman <joelroggeman@gmail.com>
2016-02-01 18:10:14 -0500

    Added minimal controller boilerplate for exam

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'exams/index'
+
+  get 'exams/new'
+
+  get 'exams/create'
+
+  get 'exams/show'
+
   resources :users, except: [:edit, :update, :destroy]
 
   get 'login' => 'sessions#new'

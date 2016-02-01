58693a26e3b931fa04a79932caf6e1a9deed6c21
Joel Roggeman <joelroggeman@gmail.com>
2016-02-01 18:09:36 -0500

    Added basic validations and tests to exam

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -1,3 +1,5 @@
 class Exam < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true
+  validates :user, presence: true
 end
--- test/models/exam_test.rb
+++ test/models/exam_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ExamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a name" do
+    e = Exam.new
+    e.name = ""
+    e.user = users(:tom)
+
+    assert_not e.save
+  end
+
+  test "should belong to someone" do
+    e = Exam.new
+    e.name = "Exam 1"
+    e.user = nil
+
+    assert_not e.save
+  end
+
+  test "should save when valid" do
+    e = Exam.new
+    e.name = "Exam 1"
+    e.user = users(:tom)
+
+    assert e.save
+  end
 end

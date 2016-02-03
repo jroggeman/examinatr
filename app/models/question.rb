270028567f5138a7ab8bfcb119f96ce9b54d58ca
Joel Roggeman <joelroggeman@gmail.com>
2016-02-03 12:30:08 -0500

    Question validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :exam
+  validates :text, presence: true
+  validates :exam, presence: true
+  validates :points, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :number, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end

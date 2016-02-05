185665f5ebca64c0a81908c55c4fbfe2067dca71
Joel Roggeman <joelroggeman@gmail.com>
2016-02-05 11:57:26 -0500

    Only need one test helper

--- test/helpers/test_helper.rb
+++
@@ -1,9 +0,0 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-
-class ActionDispatch::IntegrationTest
-  def login_as(user)
-    session[:user_id] = user.id
-  end
-end

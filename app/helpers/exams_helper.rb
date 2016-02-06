9272953ec815677669e008a2a3ff3d9408266353
Joel Roggeman <joelroggeman@gmail.com>
2016-02-06 17:55:22 -0500

    Improve code rendering

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -6,7 +6,7 @@ module ExamsHelper
   private
 
     def markdown_renderer
-      @markdown_render ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+      @markdown_render ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, disable_indented_code_blocks: false, fenced_code_blocks: true)
     end
 
 end

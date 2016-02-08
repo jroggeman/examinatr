15beb06ebb75acf8ab180e75d65235efbdfd9fdc
Joel Roggeman <joelroggeman@gmail.com>
2016-02-08 14:37:16 -0500

    Show exam total point values

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,4 +3,8 @@ class Exam < ActiveRecord::Base
   has_many :questions
   validates :name, presence: true
   validates :user, presence: true
+
+  def total_points
+    self.questions.inject(0) { |sum, q| sum + q.points }
+  end
 end

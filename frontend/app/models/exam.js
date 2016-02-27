dc6021cb0f7be05cea3bdfe4d282c762e4f7a766
Joel Roggeman <joelroggeman@gmail.com>
2016-02-27 16:11:25 -0500

    Add question badges

--- frontend/app/models/exam.js
+++ frontend/app/models/exam.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  totalPoints: DS.attr('number'),
   user: DS.belongsTo('user'),
 
   questions: DS.hasMany('question', { async: true })

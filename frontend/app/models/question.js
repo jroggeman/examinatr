f0272c84648bfc47eaf171534c006056a084d969
Joel Roggeman <joelroggeman@gmail.com>
2016-02-27 15:02:01 -0500

    Form shows up for question

--- frontend/app/models/question.js
+++ frontend/app/models/question.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   number: DS.attr('number'),
   text: DS.attr('string'),
+  answer: DS.attr('string'),
   points: DS.attr('number'),
 
   exam: DS.belongsTo('exam')

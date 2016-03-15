69bfef17035336997e0d204629fb69826c865e5a
Joel Roggeman <joelroggeman@gmail.com>
2016-03-15 17:54:08 -0400

    Remove commented code

--- frontend/app/routes/question.js
+++ frontend/app/routes/question.js
@@ -4,26 +4,4 @@ export default Ember.Route.extend({
     model: function() {
         return this.modelFor('exam').questions;
     },
-
-    /*
-    actions: {
-        createQuestion: function() {
-            var number = this.get('controller').get('number');
-            var text = this.get('controller').get('text');
-            var answer = this.get('controller').get('answer');
-            var points = this.get('controller').get('points');
-
-            var question = this.store.createRecord('question', {
-                number: number,
-                text: text,
-                answer: answer,
-                points: points,
-                exam_id: this.modelFor('exam').id
-            });
-
-            question.save();
-            this.transitionTo('exams.show.question', question);
-        }
-    }
-    */
 });

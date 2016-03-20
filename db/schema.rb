90cf794a71e37b8a6d083e3cfc5cf1d0a49b4cc4
Joel Roggeman <joelroggeman@gmail.com>
2016-03-20 14:27:12 -0400

    Refactor database.yml (and ignore production for now)

--- db/schema.rb
+++ db/schema.rb
@@ -13,74 +13,38 @@
 
 ActiveRecord::Schema.define(version: 20160222165540) do
 
-  create_table "courses", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "courses", ["name"], name: "index_courses_on_name", unique: true
-
-  create_table "exam_versions", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "exam_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "exam_versions", ["exam_id"], name: "index_exam_versions_on_exam_id"
-
   create_table "exams", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "courses_id"
+    t.string   "name",       limit: 255
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "exams", ["user_id"], name: "index_exams_on_user_id"
-
-  create_table "question_memberships", force: :cascade do |t|
-    t.integer  "exam_version_id"
-    t.integer  "question_id"
-    t.integer  "number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  add_index "question_memberships", ["exam_version_id"], name: "index_question_memberships_on_exam_version_id"
-  add_index "question_memberships", ["question_id"], name: "index_question_memberships_on_question_id"
+  add_index "exams", ["user_id"], name: "index_exams_on_user_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
-    t.integer  "number"
-    t.text     "text"
-    t.integer  "points"
-    t.integer  "exam_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text     "answer"
+    t.integer  "number",     limit: 4
+    t.text     "text",       limit: 65535
+    t.integer  "points",     limit: 4
+    t.integer  "exam_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.text     "answer",     limit: 65535
   end
 
-  add_index "questions", ["exam_id"], name: "index_questions_on_exam_id"
-
-  create_table "test_versions", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "exam_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "test_versions", ["exam_id"], name: "index_test_versions_on_exam_id"
+  add_index "questions", ["exam_id"], name: "index_questions_on_exam_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
-    t.string   "name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "api_key"
+    t.string   "username",        limit: 255
+    t.string   "password_digest", limit: 255
+    t.string   "name",            limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "api_key",         limit: 255
   end
 
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
+  add_foreign_key "exams", "users"
+  add_foreign_key "questions", "exams"
 end

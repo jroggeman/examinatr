58e4b3356030816b2a85d5d20accd3167b1484da
Joel Roggeman <joelroggeman@gmail.com>
2016-03-27 16:06:59 -0400

    Use acts_as_list for question object

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,10 @@
 class Question < ActiveRecord::Base
   belongs_to :exam
+  acts_as_list scope: :exam
+
   validates :text, presence: true
   validates :exam, presence: true
   validates :points, presence: true, numericality: { greater_than_or_equal_to: 1 }
-
-  # Don't require; assign automatically if not present
-  #validates :number, numericality: { greater_than_or_equal_to: 1 }
 
   before_create :assign_number
 

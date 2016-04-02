0c52d224af01b845e6ad7230fa783f6fda85359f
Joel Roggeman <joelroggeman@gmail.com>
2016-04-02 15:05:08 -0400

    Add moveUp to model

--- frontend/app/models/question.js
+++ frontend/app/models/question.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { memberAction } from 'ember-api-actions';
 
 export default DS.Model.extend({
   number: DS.attr('number'),
@@ -7,4 +8,9 @@ export default DS.Model.extend({
   points: DS.attr('number'),
 
   exam: DS.belongsTo('exam'),
+
+  moveUp: memberAction({
+      path: 'move_up',
+      type: 'post'
+  }),
 });

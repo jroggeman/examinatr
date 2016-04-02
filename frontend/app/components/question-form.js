9832fb1a4d987f58a326d94c7f7de5f5699cd477
Joel Roggeman <joelroggeman@gmail.com>
2016-04-02 14:54:01 -0400

    Bubble up the click action

--- frontend/app/components/question-form.js
+++ frontend/app/components/question-form.js
@@ -16,6 +16,9 @@ export default Ember.Component.extend({
         },
         deleteQuestion: function() {
             this.sendAction('deleteAction');
+        },
+        moveUp: function() {
+            this.sendAction('moveUpAction');
         }
     }
 });
